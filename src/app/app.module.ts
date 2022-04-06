1c391cfcf8fcebaa1bce08bd2690aa0b5d683bbf
LuanFernandes <luanzera.contato@gmail.com>
2022-04-05 21:39:24 -0300

    add search-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './view/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
